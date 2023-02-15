168d3338923c404386183972ce61a940ab5a3db3
github_username <you@domain.com>
2023-02-15 17:44:49 +0600

    js file added

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('hello')
+// console.log('hello');
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previescaseNumber = parseInt(caseNumberString);
+    const newCaseNumber = previescaseNumber + 1;
+    caseNumberField.value = newCaseNumber;
+})
